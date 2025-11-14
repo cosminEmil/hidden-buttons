632289ca60318672a916acc849beb10bdfb1388e
Cosmin Emil Bucur <cosminemilbucur@Cosmins-MacBook-Air.local>
2025-11-14 23:29:12 +0200

    implemented the bonus version

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-let result;
-
-function generateRandomNumber() {
-    result = Math.floor(Math.random() * 3);
-}
-
-function generateWinner() {
-    const winner = document.getElementById("winner");
-    let button1 = document.getElementById("0");
-    let button2 = document.getElementById("1");
-    let button3 = document.getElementById("2");
-    switch (result) {
-        case 0: 
-        button1.innerText = "Castigator";
-        button2.innerText = "Pierzator"
-        button3.innerText = "Pierzator";
-        break;
-
-        case 1: 
-        button1.innerText = "Pierzator";
-        button2.innerText = "Castigator"
-        button3.innerText = "Pierzator";
-        break;
-
-        case 2: 
-        button1.innerText = "Pierzator";
-        button2.innerText = "Pierzator"
-        button3.innerText = "Castigator";
-        break;
+function createButtons() {
+    const noButtons = document.getElementById("nrOfButtons").value;
+    result = Math.floor(Math.random() * (noButtons));
+    for (let i = 0; i < noButtons; ++i) {
+        let btn = document.createElement("button");
+        btn.id = i;
+        btn.className = "btn mt-0 ml-5"
+        let element = document.getElementById("buttons");
+        element.appendChild(btn);
+        btn.addEventListener("click", () => {
+            if (btn.id == result) {
+                btn.innerText = "Castigator";
+            } else {
+                btn.innerText = "Pierzator";
+            }
+            console.log("btn.id -> " + btn.id + " result -> " + result);
+        });
     }
 }
