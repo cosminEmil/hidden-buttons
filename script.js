b7ddf8cb14ed6020a615243cc0a14aef8ad83bbf
Cosmin Emil Bucur <cosminbucur2312@gmail.com>
2025-11-16 00:06:33 +0200

    Check if I fixed the 2 authors problem

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function createButtons() {
     const noButtons = document.getElementById("nrOfButtons").value;
     result = Math.floor(Math.random() * (noButtons));
     for (let i = 0; i < noButtons; ++i) {
-        let btn = document.createElement("button");
-        btn.id = i;
-        btn.className = "btn mt-0 ml-5"
+        let button = document.createElement("button");
+        button.id = i;
+        button.className = "btn mt-0 ml-5"
         let element = document.getElementById("buttons");
-        btn.addEventListener("click", () => {checkWinner(btn)});
-        element.appendChild(btn);
+        button.addEventListener("click", () => {checkWinner(button)});
+        element.appendChild(button);
     }
 }
 
-function checkWinner(btn) {
-    if (btn.id == result) {
-        btn.innerText = "Winner";
+function checkWinner(button) {
+    if (button.id == result) {
+        button.innerText = "Winner";
     } else {
-        btn.innerText = "Loser";
+        button.innerText = "Loser";
     }
 }
